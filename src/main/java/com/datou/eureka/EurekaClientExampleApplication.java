4c8d2d30db9527c8d883e55bf892903477406fe0
michael_sun <michael.sun@dyuanzi.com>
2019-10-12 17:31:21 +0800

    change for: project name refactor

--- src/main/java/com/datou/eureka/EurekaClientExampleApplication.java
+++ src/main/java/com/datou/eureka/EurekaClientExampleApplication.java
@@ -3,6 +3,8 @@ package com.datou.eureka;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @EnableEurekaClient
@@ -12,4 +14,12 @@ public class DatouEurekaClientExampleApplication {
 		SpringApplication.run(DatouEurekaClientExampleApplication.class, args);
 	}
 
+}
+
+@RestController
+class RestServive{
+	@GetMapping(path = "test")
+	public String test(){
+		return "eureka client 1";
+	}
 }
